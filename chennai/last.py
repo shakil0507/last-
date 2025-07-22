352e8f992cdbc8c0503f98bde42be6fca15cd805
shakil0507 <mohamedshakil0507@gmail.com>
2025-07-22 11:40:46 +0530

    Update last.py

--- chennai/last.py
+++ chennai/last.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Chennai Risk Chatbot", page_icon="🧠")
 st.title("🧠 Chennai Risk Chatbot")
